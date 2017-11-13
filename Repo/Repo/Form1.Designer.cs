02da45d375a4a6817b3969599f7bb9fa0362ede3
Ateeb Ahmed <aa02751@st.habib.edu.pk>
2017-11-13 09:59:48 +0500

    Blah

---
+++ Repo/Repo/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Repo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Turquoise;
+            this.button1.Location = new System.Drawing.Point(12, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Daily Report";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Turquoise;
+            this.button3.Location = new System.Drawing.Point(157, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 51);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Weekly Report";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Turquoise;
+            this.button4.Location = new System.Drawing.Point(296, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 51);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Monthly Report";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(157, 140);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(108, 32);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "Go Back";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(419, 240);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button15;
+    }
+}
+
